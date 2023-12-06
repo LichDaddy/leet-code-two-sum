df87eb3d2fe8c4cd7bd032ea491fd519a3f43132
Michael Sinclair II <140662300+LichDaddy@users.noreply.github.com>
2023-12-06 20:34:10 +0000

    Still needs input

---
+++ Main.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        class Solution {
+            public int[] twoSum(int[] nums, int target) {
+                Map<Integer, Integer> numMap = new HashMap<>();
+                int n = nums.length;
+        
+                for (int i = 0; i < n; i++) {
+                    int complement = target - nums[i];
+                    if (numMap.containsKey(complement)) {
+                        return new int[]{numMap.get(complement), i};
+                    }
+                    numMap.put(nums[i], i);
+                }
+        
+                return new int[]{}; // No solution found
+            }
+        }
+        
+    }
+}
